82be54cc4b996cb87b0944f263d7022c7b9e28da
Tunisha-Bristy <tunisha.bristy@gmail.com>
2024-01-14 01:47:25 +0600

    theme01'

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ratz/utils/theme/custom_theme/text_theme.dart';
+
+import 'custom_theme/elevated_button_theme.dart';
+
+
+class TAppTheme{
+  TAppTheme._() ;
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue ,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+  ) ;
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue ,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+  ) ;
+}
